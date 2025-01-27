9c98d4f32f15e4bfeb4ea418c296fa02aca7d761
Path <57870461+DeniedPath@users.noreply.github.com>
2025-01-27 09:29:17 -0500

    deleted some useless things

--- config/config.js
+++ config/config.js
@@ -15,7 +15,8 @@ config({ path: path.resolve(__dirname, '../.env') }); // Adjust if .env is not i
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
-    dialect: 'mysql', // Specify the dialect
+    dialect: 'mysql',
+    logging: false, // Specify the dialect
 });
 
 // Function to connect to the database
@@ -26,6 +27,7 @@ export async function connect() {
         
         // Invoke the database and table creation process
         await createDatabase(); // Call the function that starts the process of creating the database and tables
+        console.log('Database and tables synchronized');
     } catch (err) {
         console.error('Unable to connect to the database:', err);
         process.exit(1); // Exit process on server connection error
